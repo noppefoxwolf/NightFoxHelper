87ce0086ed1d08570fe1b493c5b9ad0dcaec5dab
Tomoya Hirano <tomoya.hirano@dena.com>
2016-09-10 23:45:23 +0900

    add delegate extension

---
+++ NightFoxHelper/Classes/UIApplicationDelegate+Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  UIApplicationDelegate+Extensions.swift
+//  Pods
+//
+//  Created by Tomoya Hirano on 2016/09/10.
+//
+//
+
+import UIKit
+
+public extension UIApplicationDelegate {
+  public func switchRoot(viewController: UIViewController,
+                              duration: NSTimeInterval = 0.5,
+                              options: UIViewAnimationOptions = [.TransitionFlipFromBottom, .ShowHideTransitionViews],
+                              completion: (() -> Void)? = nil) {
+    guard let window = self.window else { return }
+    guard let w = window else { return }
+    UIView.transitionWithView(w,
+                              duration: duration,
+                              options: options,
+                              animations: {
+      w.rootViewController = viewController
+    }) { (_) in
+      completion?()
+    }
+  }
+  
+  public func boot(viewController: UIViewController) -> UIWindow {
+    let window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    window.rootViewController = viewController
+    window.makeKeyAndVisible()
+    return window
+  }
+}
