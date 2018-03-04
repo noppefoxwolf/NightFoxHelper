5fb4e9e1c4003d74160dbe76a6f866dc6be59382
🦊Tomoya Hirano <noppelabs@gmail.com>
2018-03-05 03:50:48 +0900

    support custom bundle

--- NightFoxHelper/Classes/Storyboard+Extensions.swift
+++ NightFoxHelper/Classes/Storyboard+Extensions.swift
@@ -11,10 +11,10 @@ import UIKit
 public protocol StoryboardInitializable {}
 
 public extension StoryboardInitializable where Self: UIViewController {
-  public static func instantiateStoryboard() -> Self {
+  public static func instantiateStoryboard(bundle: Bundle? = nil) -> Self {
     let type = Mirror(reflecting: self).subjectType
     let name = String(describing: type).components(separatedBy: ".")[0] // クラス名を取得
-    let storyboard = UIStoryboard(name: name, bundle: nil)
+    let storyboard = UIStoryboard(name: name, bundle: bundle)
     let viewController = storyboard.instantiateInitialViewController() as! Self
     return viewController
   }
